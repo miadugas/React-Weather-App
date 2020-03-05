0f257d889deefa620fada795621ea436952f1425
Mia Dugas <miadugas@outlook.com>
2020-03-04 23:08:19 -0700

    react-weather-app

--- src/App.js
+++ src/App.js
@@ -114,14 +114,14 @@ return (
                     ))}
                 </>
             ) : (
-                  <div className="d-flex loading-spinner">
-                      <div className="spinner-border" role="status">
-                          <span className="sr-only">Loading...</span>
-                      </div>
-                  </div>
-              )}
-          </Row>
-          <Row>
+                <div className="d-flex loading-spinner">
+                    <div className="spinner-border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+            )}
+        </Row>
+        <Row>
             <Col>
                 {selectedDay ? (
                     <DayDetails
